045709b1f0417f7f15abb5c6b12e225245396027
echemendiac <cechemendia@my.apsu.edu>
2019-04-08 19:46:35 -0400

    complete rework from begining

--- app/src/main/java/com/example/assignment03_v2/MainActivity.java
+++ app/src/main/java/com/example/assignment03_v2/MainActivity.java
@@ -1,13 +1,225 @@
 package com.example.assignment03_v2;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import java.io.FileNotFoundException;
 
-public class MainActivity extends AppCompatActivity {
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Display;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import java.io.OutputStream;
+import android.content.ContentValues;
+import android.graphics.Bitmap.CompressFormat;
+import android.provider.MediaStore.Images.Media;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements OnClickListener,
+        OnTouchListener {
+
+    ImageView choosenImageView;
+    Button choosePicture;
+    Button savePicture;
+
+    Bitmap bmp;
+    Bitmap alteredBitmap;
+    Canvas canvas;
+    Paint paint;
+    Matrix matrix;
+    float downx = 0;
+    float downy = 0;
+    float upx = 0;
+    float upy = 0;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        choosenImageView = (ImageView) this.findViewById(R.id.ChoosenImageView);
+        choosePicture = (Button) this.findViewById(R.id.ChoosePictureButton);
+        savePicture = (Button) this.findViewById(R.id.SavePictureButton);
+
+        savePicture.setOnClickListener(this);
+        choosePicture.setOnClickListener(this);
+        choosenImageView.setOnTouchListener(this);
+    }
+
+    public void onClick(View v) {
+
+        if (v == choosePicture) {
+            Intent choosePictureIntent = new Intent(
+                    Intent.ACTION_PICK,
+                    android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(choosePictureIntent, 0);
+        } else if (v == savePicture) {
+            if (checkPermissionWRITE_EXTERNAL_STORAGE(this)) {
+                if (alteredBitmap != null) {
+                    ContentValues contentValues = new ContentValues(3);
+                    contentValues.put(Media.DISPLAY_NAME, "Draw On Me");
+
+                    Uri imageFileUri = getContentResolver().insert(Media.EXTERNAL_CONTENT_URI, contentValues);
+                    try {
+                        OutputStream imageFileOS = getContentResolver().openOutputStream(imageFileUri);
+                        alteredBitmap.compress(CompressFormat.JPEG, 90, imageFileOS);
+                        Toast t = Toast.makeText(this, "Saved!", Toast.LENGTH_SHORT);
+                        t.show();
+
+                    } catch (Exception e) {
+                        Log.v("EXCEPTION", e.getMessage());
+                    }
+                }
+            }
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        if (resultCode == RESULT_OK) {
+            Uri imageFileUri = intent.getData();
+            try {
+                BitmapFactory.Options bmpFactoryOptions = new BitmapFactory.Options();
+                bmpFactoryOptions.inJustDecodeBounds = true;
+                bmp = BitmapFactory.decodeStream(getContentResolver().openInputStream(
+                        imageFileUri), null, bmpFactoryOptions);
+
+                bmpFactoryOptions.inJustDecodeBounds = false;
+                bmp = BitmapFactory.decodeStream(getContentResolver().openInputStream(
+                        imageFileUri), null, bmpFactoryOptions);
+
+                alteredBitmap = Bitmap.createBitmap(bmp.getWidth(), bmp
+                        .getHeight(), bmp.getConfig());
+                canvas = new Canvas(alteredBitmap);
+                paint = new Paint();
+                paint.setColor(Color.GREEN);
+                paint.setStrokeWidth(5);
+                matrix = new Matrix();
+                canvas.drawBitmap(bmp, matrix, paint);
+
+                choosenImageView.setImageBitmap(alteredBitmap);
+                choosenImageView.setOnTouchListener(this);
+            } catch (Exception e) {
+                Log.v("ERROR", e.toString());
+            }
+        }
+    }
+
+    public boolean onTouch(View v, MotionEvent event) {
+        int action = event.getAction();
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                downx = event.getX();
+                downy = event.getY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                upx = event.getX();
+                upy = event.getY();
+                canvas.drawLine(downx, downy, upx, upy, paint);
+                choosenImageView.invalidate();
+                downx = upx;
+                downy = upy;
+                break;
+            case MotionEvent.ACTION_UP:
+                upx = event.getX();
+                upy = event.getY();
+                canvas.drawLine(downx, downy, upx, upy, paint);
+                choosenImageView.invalidate();
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
+
+
+    public static final int MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 123;
+
+    public boolean checkPermissionWRITE_EXTERNAL_STORAGE(
+            final Context context) {
+        int currentAPIVersion = Build.VERSION.SDK_INT;
+        if (currentAPIVersion >= android.os.Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(context,
+                    Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        (Activity) context,
+                        Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                    showDialog("External storage", context, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+                } else {
+                    ActivityCompat
+                            .requestPermissions(
+                                    (Activity) context,
+                                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                                    MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
+                }
+                return false;
+            } else {
+                return true;
+            }
+
+        } else {
+            return true;
+        }
+    }
+
+    public void showDialog(final String msg, final Context context,
+                           final String permission) {
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(context);
+        alertBuilder.setCancelable(true);
+        alertBuilder.setTitle("Permission necessary");
+        alertBuilder.setMessage(msg + " permission is necessary");
+        alertBuilder.setPositiveButton(android.R.string.yes,
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        ActivityCompat.requestPermissions((Activity) context,
+                                new String[] { permission },
+                                MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
+                    }
+                });
+        AlertDialog alert = alertBuilder.create();
+        alert.show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // do your stuff
+                } else {
+                    //Toast.makeText(Login.this, "GET_ACCOUNTS Denied",
+                    //Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions,
+                        grantResults);
+        }
     }
 }
